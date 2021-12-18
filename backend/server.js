db3c919a717d64e614adfef25bfd0d9f5f26e6b4
Marcinelloo <marcin.kulik12345@wp.pl>
2021-12-18 13:42:54 +0100

    conection to database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import userRauter from "./routers/userRauter.js";
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+
+app.use("/api/users", userRauter);
 
 app.get("/api/products/:id", (req, res) => {
   const product = data.products.find((x) => x._id === req.params.id);
@@ -18,6 +26,9 @@ app.get("/api/products/:id", (req, res) => {
 
 app.get("/", (req, res) => {
   res.send("Server ready");
+});
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.port || 5002;
