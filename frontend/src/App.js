2d8ef5dc71a13532857822139618ae484abbe822
Marcinelloo <marcin.kulik12345@wp.pl>
2021-12-21 14:51:23 +0100

    Adding shipping

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { signout } from "./actions/userActions";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductSreen from "./screens/ProductSreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -23,13 +33,31 @@ function App() {
                 <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            <Link to="/signin">Sign In</Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{" "}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
           </div>
         </header>
         <main>
           <Routes>
             <Route path="/cart/:id" element={<CartScreen />} exact></Route>
             <Route path="/product/:id" element={<ProductSreen />} exact />
+            <Route path="/signin" element={<SigninScreen />} exact />
+            <Route path="/register" element={<RegisterScreen />} exact />
+            <Route path="/shipping" element={<ShippingAddressScreen />} exact />
             <Route path="/" element={<HomeScreen />} exact />
           </Routes>
         </main>
